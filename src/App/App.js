7796bccd39cb5b5bd331037c5121e056fe044151
DingWeiping <Weiping@DingWeipingsMBP.gateway>
2017-11-18 09:29:16 +1000

    Initial

---
+++ src/App/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default function App() {
+	return (
+		<div style={{margin: 50}}>
+			<p>
+				To get started, edit <code>App.js</code> and save to reload.
+			</p>
+		</div>
+	)
+}
---
+++ src/Course/CourseCard.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Weiping on 18/11/17.
+ */
