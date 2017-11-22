4ece8824f1de94c0d44b9772f8e79eee78bc27b0
Yuan <daiyuan1994@hotmail.com>
2017-11-22 19:37:50 +1000

    add state

--- src/Student/StudentList.js
+++ src/Student/StudentList.js
@@ -1,11 +1,24 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 export default class StudentList extends Component{
   constructor(props){
     super(props);
     this.state = {
-
+      students:[]
     }
+  }
+
+  sendRequest(){
+    var studentURL = 'http://lazebear.azurewebsites.net/api/student';
+
+    axios.get(studentURL)
+      .then((response) => {
+        this.setState();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   render() {
