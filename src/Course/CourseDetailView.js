80555a3baf3e50edf5602b28d97d984b8838c4db
MoZhouMo <qq410431243@gmail.com>
2017-11-25 09:04:40 +1000

    course input

--- src/Course/CourseDetailView.js
+++ src/Course/CourseDetailView.js
@@ -14,8 +14,8 @@ class CourseDetailView extends Component {
 	}
 
 	isNew() {
-		const { ID } = this.props.match.params;
-		return ID === 'create';
+		const { id } = this.props.match.params;
+		return id === 'create';
 	}
 
 	componentWillMount() {
@@ -39,13 +39,121 @@ class CourseDetailView extends Component {
 			})
 	}
 
+	handleInputChange = (event) => {
+		const target = event.target;
+		const value = target.type === 'checkbox' ? target.checked : target.value;
+		const name = target.name;
+
+		this.setState({
+			course: {
+				...this.state.course,
+				[name]: value,
+			},
+		});
+	}
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+		this.setState({ isLoading: true });
+		const { course } = this.state;
+		const onSuccess = (response) => {
+			this.setState({
+				isEditing: false,
+				isLoading: false,
+				course: response.data,
+			});
+		};
+
+		if (this.props.match.params.id === 'create') {
+			axios.post('/api/course', course)
+				.then(onSuccess);
+		} else {
+			axios.put(`/api/course/${course.ID}`, course)
+				.then(onSuccess);
+		}
+	}
+
+	handleCancel() {
+		if (this.isNew()) {
+			this.props.history.push('/course');
+		} else {
+			this.setState({
+				isEditing: false,
+			});
+			this.loadCourse();
+		}
+	}
+
+	handleConfirmDelete = () => {
+		const { course } = this.state;
+
+		this.setState({ isDeleting: true });
+		axios.delete(`/api/course/${course.ID}`)
+			.then(() => {
+				this.props.history.push('/course');
+			});
+	}
+
 	renderDisplay() {
 		const { course } = this.state;
 
 		return (
-			<h1>{course.Name}</h1>
-
+			<div className="details-card">
+				<h1>{course.Name}</h1>
+				<div className="row">
+					<ul>
+						<li>{course.CourseCode}</li>
+						<li>{course.Description}</li>
+					</ul>
+				</div>
+				<div className="row">
+					<button className="btn btn-primary shadow-sm" onClick={() => this.setState({ isEditing: true })}>
+						Edit
+				</button>
+					<button className="btn btn-danger shadow-sm" onClick={this.handleConfirmDelete}>Delete</button>
+				</div>
+			</div>
 		);
+	}
+
+	renderForm = () => {
+		const { course, isSaving } = this.state;
+
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<div className="form-group">
+					<label>Name</label>
+					<input
+						type="text"
+						className="form-control"
+						placeholder="Name"
+						value={course.Name || ''}
+						name="Name"
+						onChange={this.handleInputChange}
+					/>
+					<label>CourseCode</label>
+					<input
+						type="text"
+						className="form-control"
+						placeholder="CourseCode"
+						value={course.CourseCode || ''}
+						name="CourseCode"
+						onChange={this.handleInputChange}
+					/>
+					<label>Description</label>
+					<input
+						type="text"
+						className="form-control"
+						placeholder="Description"
+						value={course.Description || ''}
+						name="Description"
+						onChange={this.handleInputChange}
+					/>
+					<button type="submit" className="btn btn-primary shadow-sm">Save</button>
+					<button className="btn btn-danger shadow-sm" onClick={() => this.handleCancel()}>Cancel</button>
+				</div>
+			</form>
+		)
 	}
 
 	render() {
