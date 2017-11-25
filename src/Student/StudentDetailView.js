a9980f1bf043a3e871a49a4d31985345feda2c1f
Yuan <daiyuan1994@hotmail.com>
2017-11-25 11:27:49 +1000

    add state

--- src/Student/StudentDetailView.js
+++ src/Student/StudentDetailView.js
@@ -4,16 +4,52 @@ export default class StudentDetailView extends Component{
   constructor(props){
     super(props);
     this.state = {
-
+      num:0,
+      student:{
+        FirstName:"",
+        LastName:"",
+        phone:"",
+        Email:""
+      }
     }
+  }
+
+  sendRequest(id){
+    var studentURL = 'http://lazebear.azurewebsites.net/api/student'+'/'+id;
+
+    axios.get(studentURL)
+      .then((response) => {
+        const student = response.data.map(function(d){
+          return{
+            ID: d.ID,
+      			FirstName: d.FirstName,
+            LastName: d.LastName,
+      			Phone: d.Phone,
+            Email:d.Email,
+            Address:d.Address
+          }
+        });
+        this.setState({students});
+
+      })
+      .catch((error) => {
+        console.log(error);
+
+      });
+  }
+
+  componentWillMount(){
+    const id = this.props.match.params.id;
+    this.setState({num:id});
   }
 
   render(){
     return(
+
       <div className="detailContainer">
         <h1>Student details</h1>
         <div className="jumbotron">
-          <h1 className="display-3">Student1</h1>
+          <h1 className="display-3">{this.state.num}</h1>
           <p className="lead">Some information</p>
           <hr className="my-4"></hr>
           <p>It uses utility classetypography and spacing to space content out within the larger container.</p>
@@ -23,6 +59,5 @@ export default class StudentDetailView extends Component{
         </div>
       </div>
     )
-
   }
 }
