5300eca43fe0200f11f4edf511ea82ecc4e1accd
Mason <mingyu.xiong@uqconnect.edu.au>
2017-12-11 23:39:42 +1000

    connect count down with backend

--- src/DashBoard/CountClock/CountClock.js
+++ src/DashBoard/CountClock/CountClock.js
@@ -1,25 +1,65 @@
 import React, { Component } from 'react';
 import './count-clock.css';
-import startCountDown from "./CountTime";
+import startCountDown, {stopCountDown} from "./CountTime";
+import Request from '../../Utils/Request';
+import {confirmAlert} from 'react-confirm-alert';
+import DatePicker from 'material-ui/DatePicker';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 export default class CountClock extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            startTime:'2017-09-19',
-            endTime:'2017-12-19'
+            StartTime:'2017-09-19',
+            EndTime:'2017-12-19'
         }
     }
 
     componentDidMount(){
-        startCountDown(this.state.endTime);
+        Request("GET", "/api/dash/time")
+            .then(r=>{
+                    console.log(r.data);
+                    this.setState({...r.data})
+                    startCountDown(this.state.EndTime);
+                }
+            ).catch(r=>{
+            console.log(r);
+        })
     }
 
+    requestTimeChange=()=>{
+        const {StartTime, EndTime} = this.state;
+        stopCountDown();
+        Request("PUT", "/api/dash/time", {StartTime, EndTime})
+            .then(r=>console.log(r))
+            .catch(e=>console.log(e))
+        startCountDown(EndTime);
 
+    }
+
+    handleTimeChange=(event, date)=>{
+        this.setState({
+            EndTime: date.toISOString().substring(0, 10)
+        });
+    }
+
+    changeTime=()=>{
+        confirmAlert({
+            title: 'Count Down Picker',                        // Title dialog
+            message: 'Pick a new date for count down',               // Message dialog
+            childrenElement: () => (<MuiThemeProvider className="dialog-content">
+                <DatePicker hintText="Pick a date" defaultDate={new Date(this.state.EndTime)} onChange={this.handleTimeChange}/>
+            </MuiThemeProvider>),       // Custom UI or Component
+            confirmLabel: 'Confirm',                           // Text button confirm
+            cancelLabel: 'Cancel',                             // Text button cancel
+            onConfirm: this.requestTimeChange,     // Action after Cancel
+        })
+    }
 
     render(){
         return(
             <div>
+                <a onClick={this.changeTime}><i className="fa fa-clock-o" aria-hidden="true"/></a>
                 <div className="countdown" id="js-countdown">
                     <div className="countdown__item countdown__item--large">
                         <div className="countdown__timer js-countdown-days" id={"cd_day"} aria-labelledby="day-countdown">
